d0d4f0562dc9470b2d137fd96928e2787bee7da2
ursmal1948 <ula.malin35@gmail.com>
2024-10-06 19:52:09 +0200

    finished implementing tests for transaction service + user service

--- tests/service/test_transaction_service.py
+++ tests/service/test_transaction_service.py
@@ -1,9 +1,13 @@
+import logging
+
 import pytest
 from unittest.mock import MagicMock
 from app.service.dto import CreateTransactionDto, TransactionDto
-from app.persistent.entity import UserEntity
+from app.persistent.entity import UserEntity, CategoryEntity
 from app.service.transactions import TransactionService
 from app.persistent.entity import TransactionEntity, IncomeEntity, ExpenseEntity
+
+logging.basicConfig(level=logging.INFO)
 
 
 @pytest.fixture
@@ -43,23 +47,20 @@ def service(mock_income_repo, mock_expense_repo, mock_transaction_repo,
     )
 
 
-def test_get_by_id(mock_income_repo, mock_expense_repo, mock_transaction_repo,
-                     mock_user_repo, mock_category_repo):
+def test_get_by_id(service, mock_transaction_repo):
     mock_transaction_repo.find_by_id.return_value = IncomeEntity(
         amount=200,
         user_id=1
     )
-    service = TransactionService(mock_income_repo, mock_expense_repo, mock_transaction_repo,
-                                 mock_user_repo, mock_category_repo)
+
     found_transaction = service.get_by_id(10)
-    assert isinstance(found_transaction, IncomeEntity)
-    assert found_transaction.amount == 200
-    assert found_transaction.user_id == 1
+    assert found_transaction['type'] == 'income'
+    assert found_transaction['amount'] == 200
+    assert found_transaction['user_id'] == 1
     mock_transaction_repo.find_by_id.assert_called_once_with(10)
 
 
-def test_add_transaction(mock_income_repo, mock_expense_repo, mock_transaction_repo,
-                         mock_user_repo, mock_category_repo):
+def test_add_transaction(service, mock_user_repo, mock_category_repo):
     mock_user_repo.find_by_id.return_value = UserEntity(
         id=1,
         name='U1',
@@ -69,13 +70,7 @@ def test_add_transaction(mock_income_repo, mock_expense_repo, mock_transaction_r
         is_active=True
     )
     dto = CreateTransactionDto(amount=100, user_id=1, category_id=10)
-    service = TransactionService(
-        mock_income_repo,
-        mock_expense_repo,
-        mock_transaction_repo,
-        mock_user_repo,
-        mock_category_repo
-    )
+
     added_transaction = service.add_transaction(dto)
     assert added_transaction is not None
     assert added_transaction['amount'] == 100
@@ -84,3 +79,64 @@ def test_add_transaction(mock_income_repo, mock_expense_repo, mock_transaction_r
 
     mock_user_repo.find_by_id.assert_called_once_with(1)
     mock_category_repo.find_by_id.assert_called_once_with(10)
+
+
+def test_create_transaction(service, mock_category_repo):
+    dto = CreateTransactionDto(amount=100, user_id=1, category_id=10)
+    mock_category_repo.find_by_id.return_value = CategoryEntity(name='C1', type_='income')
+
+    created_transaction = service.create_transaction(dto)
+    assert created_transaction is not None
+    assert isinstance(created_transaction, IncomeEntity)
+    assert created_transaction.user_id == 1
+    assert created_transaction.category_id == 10
+
+    mock_category_repo.find_by_id.assert_called_once_with(dto.category_id)
+
+
+def test_update_transaction(service, mock_transaction_repo):
+    transaction = IncomeEntity(id=1, amount=100, user_id=5, category_id=10)
+    mock_transaction_repo.find_by_id.return_value = transaction
+
+    service.update_transaction_amount(transaction_id=1, new_amount=200)
+
+    assert transaction.amount == 200
+    assert transaction.id == 1
+    assert transaction.user_id == 5
+    assert transaction.category_id == 10
+    mock_transaction_repo.find_by_id.assert_called_once_with(1)
+
+
+def test_get_all_transactions_for_category(service, mock_category_repo):
+    category = MagicMock(name='salary', type_='income')
+    mock_category_repo.find_by_name.return_value = category
+    category.income_transactions = [
+        IncomeEntity(amount=10, user_id=1, category_id=10),
+        IncomeEntity(amount=20, user_id=2, category_id=10),
+        IncomeEntity(amount=30, user_id=3, category_id=10)
+    ]
+
+    transactions = service.get_all_transactions_for_category('salary')
+
+    assert transactions is not None
+    assert len(transactions) == 3
+    assert transactions[0]['amount'] == 10
+    assert transactions[1]['amount'] == 20
+    assert transactions[2]['amount'] == 30
+    mock_category_repo.find_by_name.assert_called_once_with('salary')
+
+
+def test_get_expense_transactions_higher_than(service, mock_expense_repo):
+    transactions = [ExpenseEntity(amount=10, user_id=1, category_id=2),
+                    ExpenseEntity(amount=50, user_id=2, category_id=3),
+                    ExpenseEntity(amount=150, user_id=3, category_id=4)]
+    mock_expense_repo.find_higher_than.return_value = [transactions[1], transactions[2]]
+
+    expected_amount = 30
+    expected_transactions = service.get_transactions_higher_than(expected_amount=expected_amount,
+                                                                 transaction_type='EXPENSE')
+    assert expected_transactions is not None
+    assert len(expected_transactions) == 2
+    assert expected_transactions[0]['amount'] == 50
+    assert expected_transactions[1]['amount'] == 150
+    mock_expense_repo.find_higher_than.assert_called_once_with(expected_amount)
--- tests/service/test_user_service.py
+++ tests/service/test_user_service.py
@@ -1,78 +1,155 @@
-# import logging
-# from unittest import mock
-# from unittest.mock import MagicMock, patch
-# import pytest
-# from app.persistent.entity import UserEntity
-# from app.persistent.configuration import sa
-# from app.persistent.entity import (
-#     IncomeEntity,
-#     ExpenseEntity,
-#     UserEntity,
-#     TransactionEntity,
-#     CategoryEntity,
-#     ExpenseCategoryEntity,
-#     IncomeCategoryEntity,
-#     ActivationTokenEntity
-# )
+import logging
+from unittest import mock
+from unittest.mock import MagicMock, patch
+import pytest
+from app.persistent.entity import UserEntity
+from app.persistent.configuration import sa
+from app.persistent.entity import (
+    IncomeEntity,
+    ExpenseEntity,
+    UserEntity,
+    TransactionEntity,
+    CategoryEntity,
+    ExpenseCategoryEntity,
+    IncomeCategoryEntity,
+    ActivationTokenEntity
+)
 # from app.service.configuration import user_service
-# from app.service.dto import UserDto, CreateUserDto
-# from app.service.users import UserService
-# from app.config import DB_TEST_URL
-# from app.main import create_app
-#
-#
-# @pytest.fixture
-# def client():
-#     app = create_app()
-#     app.config.update({
-#         'TESTING': True,
-#         'SQLALCHEMY_DATABASE_URI': DB_TEST_URL
-#     })
-#
-#     with app.test_client() as client:
-#         yield client
-#
-#
-# @pytest.fixture
-# def create_user_dto() -> CreateUserDto:
-#     return CreateUserDto(
-#         name='U',
-#         email='u@gmail.com',
-#         password='****',
-#         role='admin'
-#     )
-#
-#
-# # testowanie serwis z wykorzystaniem MOCKOWANIA.
+from app.service.dto import UserDto, CreateUserDto
+from app.service.users import UserService, UserSecurityService
+from app.config import DB_TEST_URL
+from app.main import create_app
+from werkzeug.exceptions import NotFound
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config.update({
+        'TESTING': True
+    })
+
+    with app.test_client() as client:
+        yield client
+
+
+@pytest.fixture
+def create_user_dto() -> CreateUserDto:
+    return CreateUserDto(
+        name='U',
+        email='u@gmail.com',
+        password='****',
+        role='admin'
+    )
+
+
 # @pytest.fixture
 # def user_data():
 #     return {'username': 'u', 'email': 'u@gmail.com', 'password': '***', 'role': 'admin'}
-#
-#
-# @patch('app.service.users.UserService.add_user',
-#        return_value=MagicMock(id=1, username='u', email='u@gmail.com', role='admin'))
-# def test_mock_add_user(mock_add, create_user_dto):
-#     user = user_service.add_user(create_user_dto)
-#     assert user.id == 1
-#     assert user.username == 'u'
-#     assert user.role == 'admin'
-#     assert mock_add.call_count == 1
-#     mock_add.assert_called_once_with(create_user_dto)
-#
-#
-# @patch('app.service.users.UserService.get_by_id',
-#        return_value=MagicMock(id=10, username='u', email='u@gmail.com', role='admin'))
-# def test_get_user_by_id(mock_get, create_user_dto):
-#     result_user = user_service.get_by_id(10)
-#     assert result_user is not None
-#     assert result_user.username == 'u'
-#     assert result_user.email == 'u@gmail.com'
-#     mock_get.assert_called_once_with(10)
-#
-#
-# @patch('app.service.users.UserService.get_by_id',
-#        return_value=None)
-# def test_mock_get_user_by_id_not_found(mock_get, create_user_dto):
-#     result_user = user_service.get_by_id(99)
-#     assert result_user is None
-#     mock_get.assert_called_once_with(99)
+
+
+@pytest.fixture
+def example_user():
+    return {'id': 1, 'name': 'U', 'email': 'xxx@gmail.com', 'password': 'pass1', 'role': 'admin'}
+
+
+# MagicMock(id=1, username='u', email='u@gmail.com', role='admin')
+
+@pytest.fixture
+def mock_user_repo():
+    return MagicMock()
+
+
+@pytest.fixture
+def user_service(mock_user_repo):
+    return UserService(mock_user_repo)
+
+
+def test_add_user(user_service, mock_user_repo, create_user_dto):
+    mock_user_repo.find_by_name.return_value = None
+    mock_user_repo.find_by_email.return_value = None
+
+    added_user = user_service.add_user(create_user_dto)
+
+    assert added_user is not None
+    assert added_user['name'] == create_user_dto.name
+    assert added_user['email'] == create_user_dto.email
+    mock_user_repo.find_by_name.assert_called_once_with(create_user_dto.name)
+    mock_user_repo.find_by_email.assert_called_once_with(create_user_dto.email)
+
+
+def test_get_user_by_name(user_service, mock_user_repo, example_user):
+    mock_user_repo.find_by_name.return_value = MagicMock(
+        id=example_user['id'],
+        name=example_user['name'],
+        password=example_user['password'],
+        email=example_user['email'],
+        role=example_user['role']
+    )
+
+    found_user = user_service.get_by_name('U')
+    assert found_user is not None
+    assert 'password' not in found_user
+    assert found_user['email'] == 'xxx@gmail.com'
+    assert found_user['role'] == 'admin'
+    mock_user_repo.find_by_name.assert_called_once_with('U')
+
+
+def test_get_user_by_email_not_found(user_service, mock_user_repo):
+    mock_user_repo.find_by_email.side_effect = NotFound('User not found')
+
+    with pytest.raises(NotFound) as err:
+        user_service.get_by_email('exmple-email.com')
+
+    assert str(err.value) == '404 Not Found: User not found'
+
+
+def test_get_total_income(user_service, mock_user_repo, example_user):
+    mock_user_repo.calculate_total_income.return_value = 5000
+
+    total_income = user_service.get_total_income(1)
+    assert total_income == 5000
+
+
+def test_get_total_income_user_not_found(user_service, mock_user_repo):
+    mock_user_repo.find_by_id.side_effect = NotFound('User not found')
+
+    with pytest.raises(NotFound) as err:
+        user_service.get_total_income(1)
+
+    assert str(err.value) == '404 Not Found: User not found'
+
+    # @patch('app.service.users.UserService.user_repository')
+    # @patch('app.service.users.UserService.activation_token_repository')
+    # def test_mock_add_user(mock_activation_token_repo, mock_user_repo, create_user_dto):
+    #     mock_user_repo.find_by_name.return_value = MagicMock()
+    #     mock_user_repo.find_by_email.return_value = MagicMock()
+    #     user_service = UserService(mock_user_repo, mock_activation_token_repo)
+    #     user = user_service.add_user(create_user_dto)
+    #
+    #     assert user['id'] == 1
+    #     # assert user.username == 'u'
+    #     # assert user.role == 'admin'
+    #     # assert mock_user_repo.call_count == 1
+    #     # mock_user_repo.assert_called_once_with(create_user_dto)
+    #     mock_user_repo.find_by_name.assert_called_with(create_user_dto.name)
+    #     mock_user_repo.find_by_email.assert_called_with(create_user_dto.email)
+
+    # @patch('app.service.users.UserService.user_repository')
+    # # def test_add_user_email_exists(
+    # @patch('app.service.users.UserService.get_by_id',
+    #        return_value=MagicMock(id=10, username='u', email='u@gmail.com', role='admin'))
+    # def test_get_user_by_id(mock_get, create_user_dto):
+    #     result_user = user_service.get_by_id(10)
+    #     assert result_user is not None
+    #     assert result_user.username == 'u'
+    #     assert result_user.email == 'u@gmail.com'
+    #     mock_get.assert_called_once_with(10)
+    #
+    #
+    # @patch('app.service.users.UserService.get_by_id',
+    #        return_value=None)
+    # def test_mock_get_user_by_id_not_found(mock_get, create_user_dto):
+    #     result_user = user_service.get_by_id(99)
+    #     assert result_user is None
+    #     mock_get.assert_called_once_with(99)
