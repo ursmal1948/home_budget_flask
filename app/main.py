522e74cb23e14ce259509982c33bea6a82cb9ba9
ursmal1948 <ula.malin35@gmail.com>
2024-10-20 20:03:32 +0200

    order in create app, which is main function in my app, reorganized imports, blocks of code etc

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import logging
 
 from app.security.configuration import configure_security
 from app.mail.configuration import MailSender
-from app.config import MAIL_SETTINGS, JWT_CONFIG, DB_URL
+from app.config import MAIL_SETTINGS, DB_URL, SECURITY_SETTINGS
 from app.persistent.configuration import sa
 from app.routes.resource import (
     RegisterUserResource,
@@ -15,19 +15,17 @@ from app.routes.resource import (
     CreateUserResource,
     CategoryIdResource,
     CategoryNameResource,
-    TransactionResource,
+    CreateTransactionResource,
     TransactionIdResource,
     TransactionsFilterResource,
     BudgetSummaryResource,
     BudgetListSummaryResource,
     ProcessRecurringTransactionsResource,
-    CreateRecurringTransaction,
+    CreateRecurringTransactionResource,
     TransactionListByCategoryResource,
     ChangeExpenseCategoryPercentageResource,
     RecurringTransactionIdResource
 )
-
-from app.routes.users import users_blueprint
 
 logging.basicConfig(level=logging.INFO)
 
@@ -36,38 +34,39 @@ def create_app() -> Flask:
     app = Flask(__name__)
     with app.app_context():
         app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
-
         app.config['SQLALCHEMY_ECHO'] = True
-
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
         app.config.update(MAIL_SETTINGS)
-        MailSender(app, 'ula.malin35@gmail.com')
-        app.config.update(JWT_CONFIG)
-        configure_security(app)
-        sa.init_app(app)
-        migrate = Migrate(app, sa)
+        app.config.update(SECURITY_SETTINGS)
 
+        sa.init_app(app)
+
+        configure_security(app)
+        MailSender(app, 'ula.malin35@gmail.com')
+        migrate = Migrate(app, sa)
         api = Api(app)
 
-        app.register_blueprint(users_blueprint)
         api.add_resource(RegisterUserResource, '/users/register')
         api.add_resource(ActivationUserResource, '/users/activate')
         api.add_resource(UserIdResource, '/users/<int:user_id>')
         api.add_resource(UserIdTotalIncomeResource, '/users/<int:user_id>/total-income')
         api.add_resource(CreateUserResource, '/users')
+
         api.add_resource(CategoryIdResource, '/categories/<int:category_id>')
         api.add_resource(CategoryNameResource, '/categories/<string:category_name>')
-        api.add_resource(TransactionResource, '/transactions')
-        api.add_resource(TransactionsFilterResource, '/transactions/filter')
-        api.add_resource(BudgetSummaryResource, '/users/budget-summary/<int:user_id>')
-        api.add_resource(BudgetListSummaryResource, '/users/budget-summary/')
+        api.add_resource(ChangeExpenseCategoryPercentageResource, '/expense-categories-percentage/<int:category_id>')
 
-        api.add_resource(ProcessRecurringTransactionsResource, '/recurring-transactions')
-        api.add_resource(CreateRecurringTransaction, '/recurring-transaction')
+        api.add_resource(CreateTransactionResource, '/transactions')
         api.add_resource(TransactionIdResource, '/transactions/<int:transaction_id>')
         api.add_resource(TransactionListByCategoryResource, '/transactions/<string:category_name>')
-        api.add_resource(ChangeExpenseCategoryPercentageResource, '/expense-categories-percentage/<int:category_id>')
+        api.add_resource(TransactionsFilterResource, '/transactions/filter')
+
+        api.add_resource(CreateRecurringTransactionResource, '/recurring-transaction')
         api.add_resource(RecurringTransactionIdResource, '/recurring-transactions/<int:transaction_id>')
+        api.add_resource(ProcessRecurringTransactionsResource, '/recurring-transactions')
+
+        api.add_resource(BudgetSummaryResource, '/users/budget-summary/<int:user_id>')
+        api.add_resource(BudgetListSummaryResource, '/users/budget-summary/')
 
     return app
 
